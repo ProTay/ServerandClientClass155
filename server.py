34420bb6748d0357ab1bfa4ff260ef4efdb811a1
ProTay <72544483+ProTay@users.noreply.github.com>
2023-08-09 20:40:17 -0400

    Update server.py

--- server.py
+++ server.py
@@ -10,21 +10,21 @@ server.bind((ip_address, port))
 server.listen()
 
 list_of_clients = []
+nicknames = []
 
 print("Server has started...")
 
-def clientthread(conn, addr):
+def clientthread(conn, nickname):
     conn.send("Welcome to this chatroom!".encode('utf-8'))
     while True:
         try:
             message = conn.recv(2048).decode('utf-8')
             if message:
-                print ("<" + addr[0] + "> " + message)
-
-                message_to_send = "<" + addr[0] + "> " + message
-                broadcast(message_to_send, conn)
+                print(message)
+                broadcast(message, conn)
             else:
                 remove(conn)
+                remove_nickname(nickname)
         except:
             continue
 
@@ -40,9 +40,18 @@ def remove(connection):
     if connection in list_of_clients:
         list_of_clients.remove(connection)
 
+def remove_nickname(nickname):
+    if nickname in nicknames:
+        nicknames.remove(nickname)
+
 while True:
     conn, addr = server.accept()
+    conn.send('NICKNAME'.encode('utf-8'))
+    nickname = conn.recv(2048).decode('utf-8')
     list_of_clients.append(conn)
-    print (addr[0] + " connected")
-    new_thread = Thread(target= clientthread,args=(conn,addr))
+    nicknames.append(nickname)
+    message = "{} joined!".format(nickname)
+    print(message)
+    broadcast(message, conn)
+    new_thread = Thread(target= clientthread,args=(conn, nickname))
     new_thread.start()
